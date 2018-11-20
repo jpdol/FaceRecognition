f2427a367ce0f8a762c1b1d05e0c1fc49f458635
Jean Lima <jpdol.eng16@uea.edu.br>
2018-11-19 21:55:11 -0400

    Update module2_model_training.py

--- module2_model_training.py
+++ module2_model_training.py
@@ -35,7 +35,7 @@ faces,ids = getImagesAndLabels(path)
 recognizer.train(faces, np.array(ids))
 
 # Save the model into trainer/trainer.yml
-recognizer.write('trainer/trainer.yml') # recognizer.save() worked on Mac, but not on Pi
+recognizer.write('trainer/trainer.yml')
 
 # Print the numer of faces trained and end program
 print("\n [INFO] {0} faces trained. Exiting Program".format(len(np.unique(ids))))
